0342c3fae07868b25fb94316066b0e1225bfe401
MasonChen-ces <55582248+MasonChen-ces@users.noreply.github.com>
2022-10-24 16:41:01 +0800

    Update business.h

--- business.h
+++ business.h
@@ -1,17 +1,6 @@
-// enum CMD{
-//     CMD_SHELL = 1,
-//     CMD_SQL
-// };
-
-// enum TYPE{
-//     TYP_ORACLE = 1,
-//     TYP_MYSQL,
-//     TYP_POSTGRESQL
-// };
-
 #ifndef __BUSINESS__
 #define __BUSINESS__
-void do_service(int conn,char *addr, int port,char *socketFile);
+void do_service(int conn, char *addr, int port, int shmid);
 char *toshell(char *cmd,int len);
 char *toOutputFile();
 char *execute(char *shell, char *output);
